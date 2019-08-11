9e5f3c82376029fb925c20f1de1a1fc9b4131c2c
JonShort <JonShort@users.noreply.github.com>
2019-08-11 17:26:14 +0100

    Add integer information exercise

---
+++ integer-list-information/README.md
@@ -0,0 +1,16 @@
+# integer list information (in rust)
+Made at the suggestion of the [rust programming language book](https://doc.rust-lang.org/book/index.html)
+
+## How to run
+- Install [rust](https://doc.rust-lang.org/book/ch01-01-installation.html)
+- Install [cargo](https://doc.rust-lang.org/book/ch01-03-hello-cargo.html)
+
+Run the application through cargo
+```bash
+$ cargo run
+```
+
+Or run a release version by building and running directly
+```bash
+$ cargo build --release && ./target/release/integer-list-information
+```
---
+++ integer-list-information/src/main.rs
@@ -0,0 +1,65 @@
+extern crate rand;
+
+use std::collections::HashMap;
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    // populate a Vec with random numbers
+    let amount_of_numbers: i32 = rng.gen_range(25, 50);
+    let numbers: Vec<i32> = (0..amount_of_numbers).map(|_| {
+        rng.gen_range(0, 10)
+    }).collect();
+
+    println!("amount_of_numbers: {:#?}", amount_of_numbers);
+    println!("numbers: {:#?}", numbers);
+
+    // calculate the mean (sum / count)
+    let sum: i32 = numbers.iter().sum();
+    let mean: f32 = sum as f32 / amount_of_numbers as f32;
+
+    // calculate the median ('middle' of sorted list)
+    let median = calculate_median(numbers.to_vec());
+
+    // calculate the mode (num which occurs the most)
+    let mode = calculate_mode(numbers.to_vec());
+
+    println!("mean: {:#?}", mean);
+    println!("median: {:#?}", median);
+    println!("mode: {:#?}", mode);
+}
+
+fn calculate_median(mut numbers: Vec<i32>) -> f32 {
+    numbers.sort();
+    let vec_length = numbers.iter().count();
+    let has_even_length = vec_length % 2 == 0;
+    let center_index = vec_length / 2;
+
+    if has_even_length {
+        // length / 2 will resolve to x.5, which is rounded-up as i32, so we -1 for the left side
+        let left_side = &numbers[center_index - 1];
+        let right_side = &numbers[center_index];
+
+        (*left_side as  f32 + *right_side as f32) / 2 as f32
+    } else {
+        numbers[center_index] as f32
+    }
+}
+
+fn calculate_mode(numbers: Vec<i32>) -> i32 {
+    let mut map = HashMap::new();
+
+    numbers.iter().for_each(|num| {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+    });
+
+    let most_frequent_number = map
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(value, _)| *value)
+        .unwrap();
+
+    most_frequent_number
+}
